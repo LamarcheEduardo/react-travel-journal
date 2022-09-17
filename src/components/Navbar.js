40be9191f562b1e35da1cfbba7f9e7ad711fbcb5
Eduardo Lamarche <lamarcheeduardo@gmail.com>
2022-09-17 08:50:33 -0400

    fixing images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function App(){
     return (
         <div className="container">
             <div className="navbar--nav">
-                <img src="../public/images/planet-earth.png" alt="" className="Navbar--airplane"/> <p className="navbar--text">my travel journal</p>
+                <img src="./images/planet-earth.png" alt="" className="Navbar--airplane"/> <p className="navbar--text">my travel journal</p>
             </div>
         </div>
     )
